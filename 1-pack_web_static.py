afab02a574720fecca2a6c4b888319f2c348023c
frank chege <francischege602@gmail.com>
2024-02-09 01:41:12 +0300

    web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""create an archive using fabric"""
+import fabric
+from datetime import datetime
+def do_pack():
+    now = datetime.now()
+    date = now.strftime("%Y%m%d%H%M%S")
+    filename = "web_static_" + date + ".tgz"
+    if (local("tar -czvf filename /data/webstatic/*")):
+        local("mkdir /data/web_static/versions")
+        local("mv /data/web_static/*.tgz data/web_static/versions")
+        return '/data/web_static/versions'
+    else:
+        return None
