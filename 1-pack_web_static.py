f5774c7cab6204d58e74a94ca1ec9fee0443c509
frank chege <francischege602@gmail.com>
2024-02-12 17:05:44 +0300

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Create a gzipped archive containing contents of webstatic
-from fabric import Connection
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
-from os import path
+from fabric.api import local
 
 
 def do_pack():
