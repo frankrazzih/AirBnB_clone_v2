3ea4bd0a5120916c819b9ca4c3c277244f2639e0
frank chege <francischege602@gmail.com>
2024-02-12 15:18:59 +0300

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-#create an archive using fabric
+# create an archive using fabric
 from fabric import Connection
 from datetime import datetime
+from os import path
 
 
 def do_pack():
     """create an archive in the localhost."""
     local = Connection('localhost')
     now = datetime.now()
-    date = now.strftime("%Y-%m-%d-%H:%M:%S")
-    filename = "versions/web_static_" + date + ".tgz"
-    if (local.run("tar -czvf" + filename + "/webstatic/*")):
-        local.run("mkdir /web_static/versions")
-        local.run("mv /web_static/*.tgz /web_static/versions")
+    dt = now.strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_" + dt + ".tgz"
+    if (path.isdir(versions)):
+        pass
+    else:
+        local.run("mkdir /versions")
+    result = local.run("tar -czvf " + filename + "/webstatic/*")
+    if (result.exited == 0):
+        local.run("mv /web_static/*.tgz /versions")
         return filename
     else:
-        return None
+        None
+    
