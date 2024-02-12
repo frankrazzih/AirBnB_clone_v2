bce4180ce916b9915572b34b0c4c8883f5c74faa
frank chege <francischege602@gmail.com>
2024-02-12 23:29:38 +0300

    web static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# deploy an archive to a remote sever
+
+from fabric.api import Connection, Put, local, env, run
+from os import path
+
+
+web01 = "ubuntu@54.221.181.151"
+web02 = "ubuntu@100.26.169.188"
+env.hosts = [web01, web02]
+
+def do_deploy(archive_path):
+    """deploys an archive to a remote server"""
+    if not path.isfile(archive_path):
+        return False
+    ls = archive_path.split('/')
+    file_name = ls[-1]
+    uncomp_file = file_name.split('.')
+    #on the localhost
+    result1 = local("scp -o 'StrictHostKeyChecking=no' {} {}:/tmp/".format(archive_path, web01))
+    result2 = local("scp -o 'StrictHostKeyChecking=no' {} {}:/tmp/".format(archive_path, web02))
+    if (result1.failed or result2.failed):
+        return False
+    #on the servers
+    # uncompress
+    result1 = run("tar -xzvf /tmp/{} -C /data/web_static/releases/{}".format(file_name, uncomp_file))
+    #remove archive and symlinks
+    result2 = run("rm /tmp/{} && rm /data/web_static/current".format(file_name))
+    #create symlink
+    result3 = run("ln -s /data/web_static/releases/{} /data/web_static/current".format(uncomp_file))
+    if (result2.failed or result1.failed or result3.failed):
+        return False
