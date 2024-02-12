089f294071e50caa979ed55fb9b221ec7925ea16
frank chege <francischege602@gmail.com>
2024-02-12 17:10:33 +0300

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 # Fabfile to generates a .tgz archive from the contents of web_static.
 from os import path
 from datetime import datetime
-from fabric.api import Connection
+from fabric.api import local
 
 
 def do_pack():
