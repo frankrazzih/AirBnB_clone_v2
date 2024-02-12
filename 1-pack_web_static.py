e689a21ec26ab833f9fde511dd875f8573f3590c
frank chege <francischege602@gmail.com>
2024-02-12 17:24:00 +0300

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,15 +9,15 @@ def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     now = datetime.now()
     dt = now.strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_" + dt + ".tgz"
+    filename = "web_static_" + dt + ".tgz"
     dir = "versions"
     if (path.isdir(dir)):
         pass
     else:
-        local("mkdir /versions")
-    result = local("tar -czvf " + filename + "/webstatic/")
+        local("mkdir versions")
+    result = local("tar -czvf " + "$filename" + "webstatic")
     if (result.exited == 0):
-        local("mv /web_static/*.tgz /versions")
-        return filename
+        local("mv *.tgz versions")
+        return "versions/" + filename
     else:
         None
