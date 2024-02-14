c884c01fce869fcec8d68932b3b533933a14d69d
frank chege <francischege602@gmail.com>
2024-02-14 12:21:42 +0300

    web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # full deployment
 from 1-pack_web_static import do_pack
+"""create a compressed archive"""
 from 2-do_deploy_web_static import do_deploy
+"""deploys the compressed archive to remote servers"""
 
 def deploy():
     """calls dopack and do deployment"""
