8c3eae23d3771cfcfabbaec2d5f2f80e7bc700db
frank chege <francischege602@gmail.com>
2024-02-14 12:39:43 +0300

    web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,10 +5,7 @@ from 2-do_deploy_web_static import do_deploy
 
 
 def deploy():
-    """
-        Args: none
-        Returns:result of the do_deploy function
-    """
+    """Returns:result of the do_deploy function."""
     archive_path = do_pack()
     result = do_deploy(archive_path)
     return result
