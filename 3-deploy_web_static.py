ea39ec80bfad7ae389d6c652ba30a3c6796fa36f
frank chege <francischege602@gmail.com>
2024-02-14 10:57:08 +0300

    web static

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# full deployment
+from 1-pack_web_static import do_pack
+from 2-do_deploy_web_static import do_deploy
+
+def deploy():
+    """calls dopack and do deployment"""
+    archive_path = do_pack()
+    result = do_deploy(archive_path)
+    return result
